892cc3f6a351918f3367731404483c1f69271e51
quangnn <ngocquangptit139@gmail.com>
2021-12-27 22:58:08 +0700

    so sánh folder

--- src/main/java/com/ptit/similarityfile/service/Main.java
+++ src/main/java/com/ptit/similarityfile/service/Main.java
@@ -2,58 +2,49 @@ package com.ptit.similarityfile.service;
 
 
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 
 public class Main {
+	public static String  pathInput="C:\\Users\\Admin 88\\Downloads\\Data_1";
+	public static String  pathOutput="C:\\Users\\Admin 88\\Downloads\\__MACOSX";
 
-	/**
-	 * @param args
-	 */
-	private static String A;
-	private static List<String> B;
-	private static int len;
-		
-	static {
-		A = "";
-		B = new ArrayList<String>();
+	public static void main(String[] args) throws FileNotFoundException {
+
+		File file=new File(pathInput);
+		if(!file.isDirectory()){
+			System.out.println("Nhập sai thư mục");
+			return;
+		}
+		File fileOut=new File(pathOutput+"\\"+"output.csv");
+		PrintWriter printWriter=new PrintWriter(fileOut);
+		printWriter.println("STT, Source File , Compare File, Simillarity");
+		int dem=0;
+		String[] children = file.list();
+		for (int i = 0; i < children.length-1; i++) {
+			for (int j = i+1; j < children.length; j++) {
+				String file1=file.getAbsolutePath()+"\\"+children[i];
+				String file2=file.getAbsolutePath()+"\\"+children[j];
+				String result=comapreFile(file1,file2);
+				printWriter.println(dem+","+children[i]+","+children[j]+","+result);
+				dem++;
+			}
+		}
+		System.out.println("Xử lý thành công");
+		printWriter.close();
 	}
-
-
-	public static void main(String[] args) {
-        MainFrame mf = new MainFrame();
-        while(true)
-        {
-        	if(!mf.isVisible())
-        	{
-        		break;
-        	}
-        }
-//	    
-//		long time1 = System.currentTimeMillis();
-//		// 读文件
-//		len = Integer.parseInt(args[0]);
-//		A = FileToString.readFile(args[1] + "/A.cpp");
-//		for (int i = 0; i < len; i++) {
-//			B.add(FileToString.readFile(args[1] + "/B" + (i) + ".cpp"));
-//			// B.add(FileToString.readFile(args[1]+"/B ("+(i+1)+").cpp"));
-//		}
-
-//		GetAstSimilarity gas = new GetAstSimilarity();
-//		gas.SimilarityCalculation(A, B);
-//		// 获取序号和相似度
-//	//	System.out.println(gas.getSimilarity()+" "+gas.getIndex());
-//		Integer sim = (int)(gas.getSimilarity()*100);
-//		Integer idx = gas.getIndex();
-//        try {
-//			FileWriter fw = new FileWriter(args[1]+"/Similarity.out");
-//			fw.write(sim.toString()+" "+idx.toString()+"\n");
-//			fw.close();
-//		} catch (IOException e) {
-//			System.out.println("file writer FAILED");
-//		}
-//        long time2 = System.currentTimeMillis();
-      //  System.out.println("using "+(time2-time1)+"ms");
+	private static String comapreFile(String file1,String file2){
+		String fileA = FileToString.readFile(file1);
+		String fileB = FileToString.readFile(file2);
+		GetAstSimilarity gas = new GetAstSimilarity();
+		gas.SimilarityCalculation(fileA, Arrays.asList(fileB));
+		List<Double> simList = gas.getSimList();
+		return simList.get(0).toString();
 	}
 
 }
